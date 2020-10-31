be8a5be430b980614b71e3a6930b36f2b43fb01a
Maguilera85 <73233136+Maguilera85@users.noreply.github.com>
2020-10-31 18:37:37 -0300

    estamos OK
    agregué parte 2 trabajo grupal 4 =)

--- tarea4g_m2.py
+++ tarea4g_m2.py
@@ -17,3 +17,25 @@ for n in range(1,15):
 
 print(lista)
 input()
+
+lista = []
+lista2 = []
+
+for x in range(3, 26):
+    lista2.append(x)
+
+
+for n in range(2, 501):
+    lista.append(n)
+    if n % 2 == 0 and n != 2:
+        lista.remove(n)
+print(lista)
+print()
+for numero2 in lista2:
+    for numero in lista:
+        if numero % numero2 == 0:
+            lista.remove(numero)
+
+
+print()
+print(lista)
