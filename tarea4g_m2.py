28089217d33b70368f99ab8c3aceb958f058d104
francisca <franciscacruzgantes@gmail.com>
2020-10-31 13:44:11 -0300

    agrego codigo a archivo py

--- tarea4g_m2.py
+++ tarea4g_m2.py
@@ -1,1 +1,19 @@
 
+lista = []
+numero = int(input('Ingrese numero 1: '))
+lista.append(numero)
+
+for n in range(1,15):
+	numero2 = int(input('ingresa otro numero: '))
+	lista.append(numero2)
+
+	if lista[n] != lista[n-1]:
+		continue
+    
+    # falta solicitar otro numero en caso de error
+	if lista[n] == lista[n-1]:
+		print('error')
+		break
+
+print(lista)
+input()
